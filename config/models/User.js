197d2faadbf29adeb6f274749eaaec2bf777327b
Amine Smida <amine.smida1990@gmail.com>
2023-06-13 10:36:09 +0100

    first commit

---
+++ config/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  fullname: String,
+  email: String,
+  age: Number,
+});
+
+module.exports = mongoose.model("users", userSchema);
